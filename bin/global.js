e45e1e3450ae79b50e16c73da887d4f7dcf0850c
Matyi <matyas.csorba@melkweg.hu>
2019-07-08 17:12:53 +0200

    create base scss files

--- bin/global.js
+++ bin/global.js
@@ -2,4 +2,10 @@
 
 const frontend = require('../lib/index');
 
-frontend.init.init();
+if (process.argv[2] === 'init' && process.argv[3]) {
+    frontend.init.init();
+}
+
+if ((process.argv[2] === 'c' && process.argv[3] === 'm')) {
+    frontend.init.createModule(process.argv[4]);
+}
